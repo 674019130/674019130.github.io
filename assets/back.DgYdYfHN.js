80aaa8d09cadb71a2fe1b00032bfdba25bfe857d
674019130 <674019130@users.noreply.github.com>
2025-04-28 08:09:23 +0000

    deploy: 9d9ef0c7b89e6316bbc1185797ab6452fd2d9077

---
+++ assets/back.DgYdYfHN.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.DFd5EO1j.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
